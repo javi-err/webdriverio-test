5cb338dcbaee8d4403247d0e358041e0bf09aa16
javi <jsmart1993@hotmail.com>
2020-10-21 09:15:06 -0700

    completed ebay product search

--- test/specs/search.js
+++ test/specs/search.js
@@ -3,4 +3,26 @@
          browser.url('https://ebay.com/');
          expect(browser).toHaveTitle('Electronics, Cars, Fashion, Collectibles & More | eBay');
      })
+
+     it('Should search for a product and verify the search text value', () => {
+         const searchInput = $('#gh-ac');
+         const searchButton = $('#gh-btn');
+
+         searchInput.addValue('Laptop');
+         searchButton.click();
+
+         expect(searchInput).toHaveValue('Laptop');
+     })
+
+     it('Should redirect to a new page and verify the title', () => {
+         expect(browser).toHaveTitle('Laptop | eBay');
+     })
+
+     it('Should update the search dropwdown', () => {
+         const category = $('#gh-cat > option:nth-child(1)')
+
+         expect(category).toHaveText('PC Laptops & Netbooks')
+
+     })
+
  })
